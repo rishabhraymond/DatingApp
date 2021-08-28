ca6d7b3b73e31a026f6e8976a487642c9486abee
rishabhraymond <[rishabh.17bit1110@abes.ac.in]>
2021-08-29 00:17:20 +0530

    Inital commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase {
+        private readonly DBContext _context;
+
+        public UsersController(DBContext context) {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers() {
+            return  await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id) {
+            return await _context.Users.FirstOrDefaultAsync(user => user.Id == id);
+        }
+    }
+}
---
+++ API/Data/DBContext.cs
@@ -0,0 +1,16 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Data {
+    public class DBContext: DbContext {
+        public DBContext(DbContextOptions options): base(options) {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
